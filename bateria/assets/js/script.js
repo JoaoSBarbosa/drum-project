c9c189d851b766c03dbd7cef699098a4121e432b
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-08 20:44:20 -0300

    Adicionado a lógica para reproduzir um som de acordo com a tecla

--- bateria/assets/js/script.js
+++ bateria/assets/js/script.js
@@ -1,5 +1,17 @@
 // Reconhecer a tecla digitada
 
-document.body.addEventListener('keyup', (event) =>{
-   console.log(event)
-})
+document.body.addEventListener("keyup", (event) => {
+  playSound(event.code.toLowerCase());
+});
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+  // Verificando se o audio foi encontrado...
+  if (audioElement) {
+    audioElement.play();
+  }
+  if(keyElement){
+   keyElement.classList.add('active');
+  }
+}
