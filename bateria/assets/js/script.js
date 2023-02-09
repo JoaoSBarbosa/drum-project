cec6a7c471986c16d01c79bc21f936a25d0fbbe6
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-08 21:04:03 -0300

    Destacando as teclas digitadas

--- bateria/assets/js/script.js
+++ bateria/assets/js/script.js
@@ -3,15 +3,31 @@
 document.body.addEventListener("keyup", (event) => {
   playSound(event.code.toLowerCase());
 });
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+  if (song !== "") {
+    let songArray = song.split("");
+    
+    for(let i in songArray){
+      songArray[i].play();
+    }
+    
+  }
+});
 
 function playSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
-  let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
   // Verificando se o audio foi encontrado...
   if (audioElement) {
+    audioElement.currentTime = 0;
     audioElement.play();
   }
-  if(keyElement){
-   keyElement.classList.add('active');
+
+  if (keyElement) {
+    keyElement.classList.add("active");
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 400);
   }
 }
